ece9e2022263f17079f30966887aa647682d73d7
TJ Lynch <anthonyvlynch5@gmail.com>
2021-09-10 12:40:52 -0400

    client.js - added clearinputs function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -101,4 +101,13 @@ function appendToDOM(response) {
       </tr>
     `);
   };
+  clearInputs();
 };
+
+function clearInputs() {
+  $('#nameIn').val('');
+  $('#ageIn').val('');
+  $('#genderIn').val('');
+  $('#readyForTransferIn').val('');
+  $('#notesIn').val('');
+}
